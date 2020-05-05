71f132ab091587ee31e47e1b30a5b01e36438324
sainitishkumar <bsainitishkumar@gmail.com>
2020-05-05 18:59:05 +0530

    get video from client

---
+++ public/screenshare.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+
+const videoElem = document.getElementById("video");
+const startElem = document.getElementById("start");
+const stopElem = document.getElementById("stop");
+
+console.log(document.URL, videoElem, startElem, stopElem)
+// Options for getDisplayMedia()
+
+var displayMediaOptions = {
+  // video: {
+  //   cursor: "always"
+  // },
+  video: {
+    chromeMediaSource: 'desktop',
+    cursor: "always"
+  },
+  audio: false
+};
+
+// Set event listeners for the start and stop buttons
+startElem.addEventListener("click", function(evt) {
+  startCapture();
+}, false);
+
+stopElem.addEventListener("click", function(evt) {
+  stopCapture();
+}, false);
+
+async function startCapture() {  
+    try {
+      videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    } catch(err) {
+      console.error("Error: " + err);
+    }
+}
+
+function stopCapture(evt) {
+    let tracks = videoElem.srcObject.getTracks();
+    tracks.forEach(track => track.stop());
+    videoElem.srcObject = null;
+}
+}
